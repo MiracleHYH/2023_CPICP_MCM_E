7fd69ab44da91752443583004674fe9beedfb837
MiracleHYH <miracle20001110@gmail.com>
2023-09-25 11:26:34 +0800

    Changes

---
+++ solution/Q3/a/dataProcess.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import os
+from conf import dataPath
+import numpy as np
+
+table1 = pd.read_csv(os.path.join(dataPath, 'table1.csv'))
+table2 = pd.read_csv(os.path.join(dataPath, 'table2.csv'))
+table3_ed = pd.read_csv(os.path.join(dataPath, 'table3_ed.csv'))
+table3_hm = pd.read_csv(os.path.join(dataPath, 'table3_hemo.csv'))
+
+self_info = table1.iloc[:, 4:22].drop(columns=['脑出血前mRS评分', '血压'])
+pressure = table1['血压'].str.split('/', expand=True)
+mRS = table1['脑出血前mRS评分'].astype(np.int32)
+self_info['性别'] = self_info['性别'].map({'男': 1, '女': 0})
+
+vp_info = table2.iloc[:, 2:23]
+
+sd_info = []
+for i in range(160):
+    serial_number = table1.iloc[i, 3]
+    sd_info.append(
+        np.concatenate((
+            table3_ed[table3_ed['流水号'] == serial_number].iloc[0, 2:32].values,
+            table3_hm[table3_hm['流水号'] == serial_number].iloc[0, 2:32].values
+        ))
+    )
+x = pd.DataFrame(
+    np.concatenate((
+        self_info.values,
+        pressure.values,
+        vp_info.values,
+        np.array(sd_info)
+    ), axis=1).astype(np.float32)
+)
+x = x.fillna(0)
+x = (x - x.min()) / (x.max() - x.min())
+dataset = pd.DataFrame(np.concatenate((x.values, mRS.values.reshape(-1, 1)), axis=1))
+dataset.to_csv('./dataset.csv', index=False, header=False)
---
+++ solution/Q3/a/svm.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+dataset = pd.read_csv('./dataset.csv', header=None)
+
+X_train = dataset.iloc[:100, :-1].values
+y_train = dataset.iloc[:100, -1].values
+X_test = dataset.iloc[:, :-1].values
+y_test = dataset.iloc[:, -1].values
+
+# 创建 SVM 模型
+model = svm.SVC(kernel='linear')  # 选择线性 SVM，你也可以选择其他核函数
+
+# 训练 SVM 模型
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+# 计算准确度
+accuracy = accuracy_score(y_test, y_pred)
+print("准确度:", accuracy)
+
+# 打印分类报告
+print(classification_report(y_test, y_pred))
+
+# 打印混淆矩阵
+conf_matrix = confusion_matrix(y_test, y_pred)
+print("混淆矩阵:\n", conf_matrix)
